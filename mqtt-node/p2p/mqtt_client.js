b37da2d40bf9ff3688e5e2288c62a0dc82771d87
Yuhan Zhou <yuhan.zhou98@gmail.com>
2021-05-01 00:33:08 -0400

    try get and post on mqtt

--- mqtt-node/p2p/mqtt_client.js
+++ mqtt-node/p2p/mqtt_client.js
@@ -1,94 +1,32 @@
-console.log("-----mqtt client trying to start-----")
-
 var mqtt = require('mqtt')
-// var client = mqtt.connect({ port: 1883, host: '127.0.0.1', keepalive: 10000});
-// var client = mqtt.connect([{ host: 'localhost', port: 1883 }]);
-const client = mqtt.connect("mqtt://broker:1883");
 
-client.on('error', function(error) {
-    console.log(error);
-})
+function postItem(sellerId, category, detail) {
+  const topic = category + '/' + sellerId
+  const client = mqtt.connect("mqtt://broker:1883");
+  client.publish(topic, detail, { retain: true })
+}
 
-client.on('connect', function () {
-console.log('trying to connect');
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    } else {
-        console.log('connection failed!!')
-    }
+function getItem(category) {
+  const client = mqtt.connect("mqtt://broker:1883");
+  client.on('connect', function () {
+    console.log('connected');
+    client.subscribe(category + '/#', function (err) {
+      if (!err) {
+        console.log("subscribed to " + category + '/#')
+      } else {
+        console.log(err)
+      }
+    })
   })
-})
+  client.on('message', function (topic, message) {
+    if (message.toString().length > 0) {
+      console.log('item get!')
+      console.log(message.toString())
+    }
+    client.end()
+  })
+}
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
-
-
-// var mqtt = require('mqtt')
-// var client  = mqtt.connect('0.0.0.0:1883')
-
-// var host = '0.0.0.0';     // change!
-// var port = 1883;    // change according to protocol
-// var clientId = 'clientjs';  // change!
-// var topic = 'testtopic/geolocation';  // change!
-
-// var dataSender;
-// var coords;
-
-// function sendLocation() {
-//     if (!navigator.geolocation) {
-//         status.textContent = 'geo location is not supported';
-//     } else {
-//         navigator.geolocation.getCurrentPosition(
-//             function (pos) {
-//                 // TODO: Here we can compare the new coords and old coords and only send data if the coords changed a lot (Or maybe use the watchPosition function)
-//                 coords = pos.coords;
-//                 message = new Paho.MQTT.Message('(' + coords.latitude + ',' + coords.longitude + ')');
-//                 message.destinationName = topic;
-//                 mqtt.send(message);
-//                 console.log('(' + coords.latitude + ',' + coords.longitude + ')');
-//             },
-//             function (err) {
-//                 console.warn(`ERROR(${err.code}): ${err.message}`);
-//                 alert('failed to share location data');
-//                 clearInterval(dataSender);
-//             }
-//         );
-//     }
-// }
-
-// function mqttConnect() {
-//     console.log("connecting to " + host + " " + port);
-//     mqtt = new Paho.MQTT.Client(host, port, clientId);
-//     var options = {
-//         timeout: 2000,
-//         onSuccess: function () {
-//             console.log("MQTT Connected");
-//         }
-//     };
-//     mqtt.connect(options);
-// }
-
-// mqttConnect();
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     mqttConnect();
-
-//     var checkbox = document.querySelector('#geoSwitch');
-
-//     checkbox.addEventListener('change', function () {
-//         if (checkbox.checked) {
-//             console.log('geo location enabled');
-//             dataSender = setInterval(sendLocation, 5000);
-//         } else {
-//             console.log('geo location disabled');
-//             clearInterval(dataSender);
-//         }
-//     });
-// });
-
-
+postItem(1234, 'book', 'Jane Austen')
+getItem('book')
+getItem('book')
