aa20dcfeecd4953d450c2b7bc74073b4cb449aab
Chuhan Chen <yourgitemail@example.com>
2021-05-02 16:27:54 -0400

    Update

---
+++ LoginRegister/main.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask import Response, send_file
+
+app = Flask(__name__)
+
+import sql as db
+
+@app.route('/')
+
+def index():
+    return render_template("login.html")
+
+
+@app.route('/register')
+def new_user():
+    return render_template("register.html")
+#
+# @app.route('/profile')
+# def profile():
+#     return render_template("profile.html")
+
+@app.route('/insert', methods=['post'])
+def insert():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        gender = request.form['gender']
+        age = request.form['age']
+        db.insert_users(username, password, firstname, lastname, gender, age)
+        # details = db.get_users()
+        # print(details)
+        # for detail in details:
+        #     var = detail
+        #     return render_template('register.html', var=var)
+        #
+        return render_template("login.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ LoginRegister/sql.py
@@ -0,0 +1,38 @@
+import pymysql
+
+conn = pymysql.connect(
+    host = "cc-instance.c75aookn21ch.us-east-2.rds.amazonaws.com",
+    port = 3306,
+    user = "admin",
+    password = "Zj19660412!",
+    db = "cloudComputing",
+)
+#Table Creation
+cursor=conn.cursor()
+
+def insert_users(username, password, firstname, lastname, gender, age):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Users(username, password, firstname, lastname, gender, age) VALUES(%s, %s, %s, %s, %s, %s)", (username, password, firstname, lastname, gender, age))
+    conn.commit()
+
+def get_users():
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM Users")
+    users = cur.fetchall()
+    return users
+
+if __name__ == "__main__":
+    conn = pymysql.connect(
+        host = "cc-instance.c75aookn21ch.us-east-2.rds.amazonaws.com",
+        port = 3306,
+        user = "admin",
+        password = "Zj19660412!",
+        db = "cloudComputing",
+    )
+    #Table Creation
+    cursor=conn.cursor()
+    create_table="""
+    create table Users (username varchar(200), password varchar(200), firstname varchar(200), lastname varchar(200), gender varchar(200), age varchar(200))
+    """
+
+    cursor.execute(create_table)
