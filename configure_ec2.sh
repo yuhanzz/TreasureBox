b5ca3522fee27ca522745f7f33101bf3bdb99364
Yuhan Zhou <yuhan.zhou98@gmail.com>
2021-05-10 04:59:53 +0000

    set up script for ec2

---
+++ configure_ec2.sh
@@ -0,0 +1,6 @@
+sudo yum update -y
+sudo yum groupinstall "Development Tools" -y
+sudo amazon-linux-extras install docker -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
